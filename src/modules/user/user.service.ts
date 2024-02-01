671eea0c0400d54d9a8f6139a728dbaf67f83394
PawleN-D <sirpawle@gmail.com>
2024-02-01 16:34:32 +0200

    adjust import

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,5 @@
 import prisma from "../../utils/prisma";
+import { CreateUserInput } from "./user.schema";
 
 export async function createUser(input: CreateUserInput) {
     const user = await prisma.user.create({
